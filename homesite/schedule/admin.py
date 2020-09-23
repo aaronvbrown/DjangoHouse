78e6c6cd3e73e0be1b91bde441523a3d2c12c6af
Aaron Brown <worldofaaron@hotmail.com>
2020-09-23 16:55:54 -0700

    Schedule Tables

--- homesite/schedule/admin.py
+++ homesite/schedule/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import School, Teacher, Student, Course, Period
+
+admin.site.register(School)
+admin.site.register(Teacher)
+admin.site.register(Student)
+admin.site.register(Course)
+admin.site.register(Period)
